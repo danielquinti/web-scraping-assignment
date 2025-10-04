6614193448b1c6c06064e1c6b96df1ba8afbef25
CodeMinstrel <braisrg2002@gmail.com>
2025-10-04 03:39:53 +0200

    Creado campos extra stats y habilidades y refinados los actuales

--- prueba/prueba/spiders/poke_spider.py
+++ prueba/prueba/spiders/poke_spider.py
@@ -11,9 +11,20 @@ class PokeSpider(scrapy.Spider):
         item = PokeItem()
         item['number'] = card.css('span#numeronacional::text').get()
         item['name'] = card.css('div#nombrepokemon.titulo::text').get()
-        item['gen'] = card.css('div.vnav_datos table.datos.resalto tbody tr:nth-child(2) td a::text').get()
-        item['category'] = card.css('div.vnav_datos table.datos.resalto tbody tr:nth-child(3) td::text').get()
-        item['types'] = card.css('div.vnav_datos table.datos.resalto tbody tr:nth-child(4) td span a::attr(title)').getall()
+        item['gen'] = card.css('tr[title="Generación en la que apareció por primera vez"] td a::text').get()
+        item['category'] = card.css('tr[title="Categoría"] td::text').get().strip()
+        item['types'] = [x.replace("Tipo ", "").capitalize() for x in card.css('tr[title="Tipos a los que pertenece"] td span a::attr(title)').getall()]
+        
+        item['abilities'] = card.css('tr[title="Habilidades que puede conocer"] td a::attr(title)').getall()
+        item['hidden_abilities'] = card.css('tr[title="Habilidad oculta"] td a::attr(title)').get()
+        item['egg_groups'] = [x.strip() for x in card.css('tr[title="Grupos de Pokémon con los que puede reproducirse"] td::text').getall()]
+        
+        item['ps'] = response.css('td[style="width:3em"]').xpath('ancestor::tbody').xpath('.//tr[2]/td/text()').get().strip()
+        item['atk'] = response.css('td[style="width:3em"]').xpath('ancestor::tbody').xpath('.//tr[3]/td/text()').get().strip()
+        item['df'] = response.css('td[style="width:3em"]').xpath('ancestor::tbody').xpath('.//tr[4]/td/text()').get().strip()
+        item['atk_sp'] = response.css('td[style="width:3em"]').xpath('ancestor::tbody').xpath('.//tr[5]/td/text()').get().strip()
+        item['df_sp'] = response.css('td[style="width:3em"]').xpath('ancestor::tbody').xpath('.//tr[6]/td/text()').get().strip()
+        item['vel'] = response.css('td[style="width:3em"]').xpath('ancestor::tbody').xpath('.//tr[7]/td/text()').get().strip()
 
         yield item
 
